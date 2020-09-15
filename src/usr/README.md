f858b23a8929badd713094a80c69fbec7652dd96
reactuibuilder <reactuibuilder@gmail.com>
2020-09-15 20:35:09 +0300

    Embed framework

--- src/usr/README.md
+++ src/usr/README.md
@@ -1,1 +1,5 @@
-#### TODO: add the howto illustrations here as the last thing before publishing
+### Create new page
+
+<a href="https://raw.githubusercontent.com/react-ui-builder/wcd-tailwind/master/imgages/create-page.png" target="_blank">
+<img src="https://raw.githubusercontent.com/react-ui-builder/wcd-tailwind/master/imgages/create-page.png" />
+</a>
--- src/usr/section/Div.comp.tsx
+++ src/usr/section/Div.comp.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import HtmlElement from '../commons/HtmlElement';
-import { SectionProps, SectionTypes } from '../commons/Section.props';
 import pickWithValues from '../utils/pickWithValues';
+import { GlobalProps, GlobalTypes } from '../commons/Global.props';
+import PropTypes from 'prop-types';
+
+
+export interface DivProps {
+    id?: string;
+    globalAttrs?: GlobalProps;
+    backgroundImageUrl?: string;
+    tailwindUtilities?: any;
+    children?: React.ReactNode;
+}
 
 /**
  * This is a sample component, it shows how to create function wrappers.
  */
-const Div = ({id, tailwindUtilities, children, globalAttrs}: SectionProps) => {
-    const globalAttrsProps = pickWithValues(globalAttrs);
+const Div = ({id, tailwindUtilities, backgroundImageUrl, children, globalAttrs}: DivProps) => {
+    const globalAttrsProps: any = pickWithValues(globalAttrs);
+    if (backgroundImageUrl && backgroundImageUrl.length > 0) {
+        globalAttrsProps.style = globalAttrsProps.style || {};
+        globalAttrsProps.style.backgroundImage = `url(${backgroundImageUrl})`;
+    }
     return (
         <HtmlElement id={id} tag="div" tailwindUtilities={tailwindUtilities} {...globalAttrsProps}>
             {children}
@@ -15,7 +29,25 @@ const Div = ({id, tailwindUtilities, children, globalAttrs}: SectionProps) => {
     );
 };
 
-Div.propTypes = SectionTypes;
+Div.propTypes = {
+    /**
+     * Global attributes are attributes common to all HTML elements;
+     * they can be used on all elements, though they may have no effect on some elements.
+     */
+    globalAttrs: PropTypes.shape(GlobalTypes),
+    /**
+     * URL for the background image
+     */
+    backgroundImageUrl: PropTypes.string,
+    /**
+     * Tail Wind Classes
+     */
+    tailwindUtilities: PropTypes.object,
+    /**
+     * An array of the nested HTML elements
+     */
+    children: PropTypes.arrayOf(PropTypes.element),
+};
 
 Div.defaultProps = {
     children: [
